14cf8b71133da6073653984e7f9dde394a900a51
Martin Carvalho <martincarvalho@gmail.com>
2019-04-26 14:30:49 -0300

    Major improvment on Gulpfile in order to work with local depencies such as jQuery. Improves file for further integration with WordPress or any other 3rd party tool/cms.

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,93 @@
-const gulp = require("gulp");
-const browsersync = require("browser-sync").create();
-const sass = require("gulp-sass");
-const plumber = require("gulp-plumber");
-const notify = require("gulp-notify");
-const beeper = require("beeper");
+// Load all the modules from package.json
+var gulp = require("gulp"),
+  plumber = require("gulp-plumber"),
+  autoprefixer = require("gulp-autoprefixer"),
+  uglify = require("gulp-uglify"),
+  rename = require("gulp-rename"),
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync").create();
 
-function plumbError() {
-  return plumber({
-    errorHandler: function(err) {
-      notify.onError({
-        templateOptions: {
-          date: new Date()
-        },
-        title: "Gulp error in " + err.plugin,
-        message: err.formatted
-      })(err);
-      beeper();
-      this.emit("end");
-    }
-  });
-}
+var config = {
+  nodeDir: "./node_modules"
+};
 
-function css() {
+// Default error handler
+var onError = function (err) {
+  console.log("An error occured:", err.message);
+  this.emit("end");
+};
+
+// JS to watch
+var jsFiles = ["./src/**/*.js"];
+
+// Sass files to watch
+var cssFiles = ["./src/scss/**/*.scss"];
+
+// automatically reloads the page when files changed
+var browserSyncWatchFiles = [
+  "./dist/assets/css/*.css",
+  "./dist/assets/js/**/*.min.js",
+  "./dist/*.php",
+  "./dist/*.html"
+];
+
+// see: https://www.browsersync.io/docs/options/
+var browserSyncOptions = {
+  watchTask: true,
+  proxy: "http://localhost/clients/the-client-name"
+};
+
+gulp.task("scripts", function () {
   return gulp
-    .src(["src/scss/*"])
-    .pipe(plumbError())
-    .pipe(sass())
-    .pipe(gulp.dest("dist/assets/css"))
-    .pipe(browsersync.stream());
-}
+    .src(["node_modules/tether/dist/js/tether.js", "node_modules/popper.js/dist/umd/popper.js", "node_modules/jquery/dist/jquery.js", "node_modules/bootstrap/dist/js/bootstrap.js", "src/js/*.js"])
+    .pipe(gulp.dest("./dist/assets/js"))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(gulp.dest("./dist/assets/js"))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
 
-function js() {
+// Sass - Creates a regular and minified .css file in root
+gulp.task("sass", function () {
   return gulp
-    .src(["node_modules/bootstrap/dist/js/bootstrap.js", "src/js/*.js"])
-    .pipe(plumbError())
-    .pipe(gulp.dest("dist/assets/js"))
-    .pipe(browsersync.stream());
-}
+    .src("./src/scss/*.scss")
+    .pipe(plumber())
+    .pipe(
+      sass({
+        errLogToConsole: true,
+        precision: 8,
+        noCache: true
+      }).on("error", sass.logError)
+    )
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("./dist/assets/css/"))
+    .pipe(sass({
+      outputStyle: "compressed"
+    }).on("error", sass.logError))
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(gulp.dest("./dist/assets/css/"))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
 
-function browserSync(done) {
-  browsersync.init({
-    server: "./dist"
-  });
-  done();
-}
+// Starts browser-sync task for starting the server.
+gulp.task("browser-sync", function () {
+  browserSync.init(browserSyncWatchFiles, browserSyncOptions);
+});
 
-function watchFiles() {
-  gulp.watch(["src/scss/*.scss"], css);
-  gulp.watch(["src/js/*.js"], js);
-  gulp.watch("dist/*.html").on("change", browsersync.reload);
-}
+// Start the livereload server and watch files for change
+gulp.task("watch", function () {
+  // don't listen to whole js folder, it'll create an infinite loop
+  gulp.watch(jsFiles, gulp.parallel("scripts"));
 
-const build = gulp.series(gulp.parallel(css, js, browserSync), watchFiles);
+  gulp.watch(cssFiles, gulp.parallel("sass"));
+});
 
-exports.default = build;
+gulp.task("default", gulp.parallel("watch", "scripts", "sass", "browser-sync"));
